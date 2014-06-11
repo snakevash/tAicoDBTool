ff92ab5f0f8583c3d8965547c27766d16d249682
snakevash <164011252@qq.com>
2014-06-11 10:18:15 +0800

    工具集基础文件

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 
+ * User: snake
+ * Date: 14-6-3
+ * Time: 下午1:53
+ */
+require_once 'vendor/autoload.php';
+
+$objPHPExcel = PHPExcel_IOFactory::load('test.xlsx');
+
+$sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
+//var_dump($sheetData);
+echo '读出1B:' . $sheetData[1]['B'] . PHP_EOL;
+echo '循环便利几个值:' . PHP_EOL;
+for($i=6;$i<10;$i++){
+    echo '第' . $i . '行:' . PHP_EOL;
+    print_r($sheetData[$i]);
+}
