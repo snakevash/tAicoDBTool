8dad717e83b6e0e04f3804d5eaf4d72023ed294c
snakevash <164011252@qq.com>
2014-07-28 10:45:44 +0800

    增加三个入口文件

---
+++ seriesImport.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 工具启动文件
+ *
+ * User: snake
+ * Date: 14-6-3
+ * Time: 下午1:53
+ */
+require_once 'vendor/autoload.php';
+
+# 导入品牌文件
+# 导入品牌第一个文件
+//$sBrand = new \Snake\Services\BrandServices();
+//$sfiles = \Snake\FileInfo::getFilePathInfo(OtherConfig::BRANDS);
+//$sBrand->runInsertBrandMain($sfiles[0]);
+
+# 导入所有的红外代码
+# 遍历codebasebefore下所有的红外代码
+//$sCodeBaes = new \Snake\Services\CodebaseServices();
+//$files = \Snake\FileInfo::getFilePathInfo(OtherConfig::CODEBASEBEFORE);
+//foreach ($files as $file) {
+//    $r = $sCodeBaes->runInsertCodebaseMain($file);
+//}
+
+# 导入系列
+$sSeries = new \Snake\Services\SeriesServices();
+$sfiles = \Snake\FileInfo::getFilePathInfo(OtherConfig::BRANDBEFORE);
+foreach ($sfiles as $file) {
+    $r = $sSeries->runInsertSeriesMain($file);
+}
